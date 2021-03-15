13876a9ddd91dc6500553c6d0ad847c99153931f
BrunoOsio <osiobruno716@gmail.com>
2021-03-15 00:27:14 -0300

    database folder is now inside src

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 
 const PORT = process.env.PORT || 8080
 const teamController = require("./src/api/controller/teamController")
-const mongo = require("./database/connection")
+const mongo = require("./src/database/connection")
 
 app.use('/teams', teamController)
 
